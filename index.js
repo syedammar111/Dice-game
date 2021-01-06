62db6d83488e108df0668f0bc4a97ce9d4cf7d1b
syedammarilyas111 <syedammarilyas@hotmail.com>
2021-01-06 14:55:05 +0500

    many changes

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1*6;
-randomNumber1 = Math.ceil(randomNumber1);
+randomNumber1 = randomNumber1*6; // From 1-6
+randomNumber1 = Math.ceil(randomNumber1); // For whole numbers
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
 
 var randomNumber2 = Math.random();
-randomNumber2 = randomNumber2*6;
-randomNumber2 = Math.ceil(randomNumber2);
+randomNumber2 = randomNumber2*6; // From 1-6
+randomNumber2 = Math.ceil(randomNumber2); // For whole numbers
+var randomImageSource2 = "images/dice" +  randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
 
 
 // console.log("You got: " + randomNumber1 );
@@ -12,10 +23,13 @@ randomNumber2 = Math.ceil(randomNumber2);
 if(randomNumber1>randomNumber2){
   document.querySelector("h1").innerHTML= "<strong>Player 1 wins</strong>";
 }
+
+else if (randomNumber1 == randomNumber2)
+{
+  document.querySelector("h1").innerHTML= "<strong>Tie</strong>";
+
+}
 else
 {
   document.querySelector("h1").innerHTML= "<strong>Player 2 wins</strong>";
 }
-
-
-document.querySelector("img1").setAttribute("src","images/dice1.png");
